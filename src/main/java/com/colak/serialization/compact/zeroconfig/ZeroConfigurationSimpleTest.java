ff94a290dbf4156be882ab8c30d0e36cea409e66
Orçun Çolak <orcun.colak@hazelcast.com>
2024-03-14 09:45:46 +0300

    zero config serialization example

--- src/main/java/com/colak/serialization/compact/zeroconfig/ZeroConfigurationSimpleTest.java
+++ src/main/java/com/colak/serialization/compact/zeroconfig/ZeroConfigurationSimpleTest.java
@@ -1,4 +1,4 @@
-package com.colak.serialization.compact.serializer_value_with_uuid;
+package com.colak.serialization.compact.zeroconfig;
 
 import com.hazelcast.client.HazelcastClient;
 import com.hazelcast.client.config.ClientConfig;
@@ -6,21 +6,22 @@ import com.hazelcast.config.Config;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Test to show that compact serializer works for value object that has a UUI field
+ * Test to show that zero configuration works
  */
-class SerializerConfigurationTest {
+class ZeroConfigurationSimpleTest {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SerializerConfigurationTest.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(ZeroConfigurationSimpleTest.class);
 
     private static final String MAP_NAME = "uuid_map";
 
-    private static final Integer KEY = 1;
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         LOGGER.info("Starting Test");
 
         // Start server
@@ -49,16 +50,24 @@ class SerializerConfigurationTest {
     private static HazelcastInstance getHazelcastClientInstanceByConfig() {
         ClientConfig clientConfig = new ClientConfig();
 
-        clientConfig.getSerializationConfig()
-                .getCompactSerializationConfig()
-                .addSerializer(new UUIDValueObjectSerializer());
-
         return HazelcastClient.newHazelcastClient(clientConfig);
     }
 
     private static void populateMap(HazelcastInstance hazelcastClient) {
-        IMap<Integer, UUIDValueObject> myMap = hazelcastClient.getMap(MAP_NAME);
-        myMap.put(KEY, UUIDValueObject.createNew());
+        IMap<Long, Employee> myMap = hazelcastClient.getMap(MAP_NAME);
+        long id = 1L;
+        Employee employee = new Employee(id, "John Doe");
+        myMap.set(id, employee);
+        Employee employeeFromMap = myMap.get(id);
+        LOGGER.info("employeeFromMap : {}", employeeFromMap);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @ToString
+    private static class Employee {
+        private long id;
+        private String name;
     }
 
 }
