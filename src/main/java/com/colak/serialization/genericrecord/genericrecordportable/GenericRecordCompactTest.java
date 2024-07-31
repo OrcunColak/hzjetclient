2123a43aaee85fa9468808e6913c15bc4e776fc9
Orçun Çolak <orcun.colak@hazelcast.com>
2024-07-31 10:42:44 +0300

    GenericRecord compact example

---
+++ src/main/java/com/colak/serialization/genericrecord/genericrecordportable/GenericRecordCompactTest.java
@@ -0,0 +1,18 @@
+package com.colak.serialization.genericrecord.genericrecordportable;
+
+import com.hazelcast.nio.serialization.genericrecord.GenericRecord;
+import com.hazelcast.nio.serialization.genericrecord.GenericRecordBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class GenericRecordCompactTest {
+
+    public static void main(String[] args) {
+        GenericRecord genericRecord = GenericRecordBuilder.compact("person")
+                .setInt32("id", 1)
+                .setString("firstName", "a")
+                .setString("lastName", "b")
+                .build();
+        log.info("GenericRecord : {}", genericRecord);
+    }
+}
